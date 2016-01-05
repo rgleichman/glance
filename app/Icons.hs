3d28682606ebb4a0527b0d5898afb6800c0cead2
Robbie Gleichman <rgleichman@gmail.com>
2016-01-04 23:26:30 -0800

    Add textBox and connections

--- app/Icons.hs
+++ app/Icons.hs
@@ -4,7 +4,13 @@ module Icons
     apply0Icon,
     Icon(..),
     drawIconAndPorts,
-    drawIconsAndPortNumbers
+    drawIconsAndPortNumbers,
+    PortName(..),
+    nameDiagram,
+    connectPorts,
+    connectIconToPort,
+    connectIconToIcon,
+    textBox
     ) where
 
 import Diagrams.Prelude
@@ -15,20 +21,43 @@ import Diagrams.Backend.SVG.CmdLine
 -- proceed counterclockwise.
 data Icon a = Icon {iconDia :: Diagram a, ports :: [P2 Double]}
 
+-- | PortName is a simple wrapper around Int that is used for the diagram names
+-- of all the ports.
+instance IsName PortName
+newtype PortName = PortName Int deriving (Show, Ord, Eq)
+
+-- | Names the diagram and puts all sub-names in the namespace of the top level name.
+nameDiagram name dia = name .>> (dia # named name)
+
+arrowOptions = with & arrowHead .~ noHead & shaftStyle %~ lw veryThick . lc white
+
+connectPorts icon0 port0 icon1 port1 =
+  connect'
+  arrowOptions
+  (icon0 .> port0)
+  (icon1 .> port1)
+
+connectIconToPort icon0 icon1 port1 =
+  connectOutside' arrowOptions icon0 (icon1 .> port1)
+
+connectIconToIcon =
+  connectOutside' arrowOptions
+
 -- | Draw the icon with circles where the ports are
 drawIconAndPorts :: Icon B -> Diagram B
-drawIconAndPorts (Icon dia port) =
+drawIconAndPorts (Icon dia ports) =
   vertCircles <> dia
   where
-    vertCircles = atPoints port $ repeat $ circle 0.05 # lw none # fc blue
+    vertCircles = atPoints ports $ repeat $ circle 0.05 # lw none # fc blue
 
 drawIconsAndPortNumbers :: Icon B -> Diagram B
-drawIconsAndPortNumbers (Icon dia port) =
+drawIconsAndPortNumbers (Icon dia ports) =
   portNumbers <> dia
   where
-    portNumbers = atPoints port $ map makeNumDia [0,1..]
+    portNumbers = atPoints ports $ map makeNumDia [0,1..]
     makeNumDia num = text (show num) # fontSize (local 0.1) # fc blue <> square 0.1 # fc white
 
+-- APPLY 0 ICON --
 circleRadius = 0.5
 apply0LineWidth = 0.25
 
@@ -42,10 +71,16 @@ apply0Line :: Diagram B
 apply0Line = rect apply0LineWidth (2 * circleRadius) # fc white # lw none
 
 apply0Dia :: Diagram B
-apply0Dia = resultCircle ||| apply0Line ||| apply0Triangle
+apply0Dia = (resultCircle ||| apply0Line ||| apply0Triangle) <> makePortDiagrams verts
 
 apply0Icon :: Icon B
 apply0Icon = Icon apply0Dia verts
+
+makePortDiagrams points =
+  atPoints points (map makePort [0,1..])
+  where
+    --makePort x = (circle 0.2 # fc green) # named (PortName x)
+    makePort x = mempty # named (PortName x)
 
 verts = map p2 [
   (circleRadius + apply0LineWidth + triangleWidth, 0),
@@ -55,3 +90,16 @@ verts = map p2 [
   where
     triangleWidth = circleRadius * sqrt 3
     lineCenter = circleRadius + (apply0LineWidth / 2.0)
+
+-- TEXT ICON --
+textBoxFontSize = 1
+monoLetterWidthToHeightFraction = 0.6
+textBoxHeightFactor = 1.1
+textBox :: String -> Diagram B
+textBox t =
+  text t # fc white # font "freemono" # fontSize (local textBoxFontSize)
+  <> rect rectangleWidth (textBoxFontSize * textBoxHeightFactor) # lc white
+  where
+    rectangleWidth = textBoxFontSize * monoLetterWidthToHeightFraction
+      * fromIntegral (length t)
+      + (textBoxFontSize * 0.2)
--- app/Main.hs
+++ app/Main.hs
@@ -10,5 +10,16 @@ import Icons
 ex1 = drawIconAndPorts apply0Icon
 ex2 = drawIconsAndPortNumbers apply0Icon
 
+applyDia = iconDia apply0Icon
+--apply0A = "A" .>> applyDia
+apply0A = applyDia # nameDiagram "A"
+apply0B = applyDia # nameDiagram "B"
+ex3 = atPoints (map p2 [(0,0), (3,0)]) [apply0A, apply0B]
+fromAtoB = ex3 # connectPorts "A" (PortName 0) "B" (PortName 2)
+ex4 = apply0A ||| textBox "hello world" === textBox "1" === textBox "gpq" === textBox ['A'..'Z']
+ex5 = textBox "baz" # named "baz"||| hrule 1 ||| fromAtoB ||| hrule 1 ||| textBox "foo" # named "foo" === vrule 1 === textBox "bar" # named "bar"
+ex6 = ex5 # connectIconToPort "baz" "A" (PortName 2) # connectIconToPort "foo" "B" (PortName 0)
+  # connectIconToPort "bar" "B" (PortName 3)
+
 main :: IO ()
-main = mainWith (ex2 # frame 0.1 # bg black)
+main = mainWith (ex6 # frame 0.1 # bg black)
