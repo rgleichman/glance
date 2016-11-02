e8d0039b7e885381dcd30000c35eba3c74509352
Robbie Gleichman <rgleichman@gmail.com>
2016-11-01 17:58:37 -0700

    Move todos out of app/Main.hs and into todo.txt.

--- app/Main.hs
+++ app/Main.hs
@@ -13,40 +13,6 @@ import Rendering(renderDrawing)
 import Translate(drawingsFromModule)
 
 
--- TODO Now --
--- Rewrite Translate to generate an abstract computation graph that is then transformed (eg. find tree sections)
--- - and turned into an Icon graph.
--- Fix icon nesting if a non-nestable icon (eg. flatLambdaIcon) is part of the expression.
--- - eg. y = f $ g (\x -> x)
-
--- TODO Later --
--- Add documentation.
-
--- Visual todos:
--- Fix rotation missing edges to nested diagrams.
--- Don't rotate text and nested icons, give them rectangualar bounding boxes in GraphViz. (Perhaps use a typeclass for isRotateAble)
--- Give lines a black border to make line crossings easier to see.
--- Give lines that cross the border of a lambda function a special color.
--- Line intersections should have a small circle. This could probably be done with
--- a line ending.
--- Let each bool, value pair in Guard icon be flipped to reduce line crossings. Do the same for case.
--- Let lines connect to ports in multiple locations (eg. case value, or guard result)
--- Rotate icons based on the outgoing line's difference from ideal angle, not line distance.
--- Improve line routing. Draw curved lines with outgoing lines at fixed angles.
---  - connectPerim might be useful for this.
--- For nested apply, cycle through different colors and line styles (eg. dashed, solid, wavy)
--- - for each nesting level. This will help distinguish what is an argument to which funciton.
--- Investigate arrows not being drawn
-
--- Translate todos:
--- Make nested version of FlatLambdaIcon
--- Fix test case x of {0 -> 1; y -> y}.
--- Add proper RecConstr, and RecUpdate support.
--- Eliminate BranchIcon in Alts.
--- Eliminate BranchIcon for the identity funciton "y x = x"
--- Add a maximum nesting depth.
--- Special case for otherwise.
-
 renderFile :: String -> String -> IO (Diagram B)
 renderFile inputFilename includeComments = do
   parseResult <- Exts.parseFileWithComments
