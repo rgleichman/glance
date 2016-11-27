4371f2d4c6ac03635cdd6815cb59160b57ed2fbc
Robbie Gleichman <rgleichman@gmail.com>
2016-11-27 15:07:35 -0800

    Fix factorial test.

--- test/AllTests.hs
+++ test/AllTests.hs
@@ -376,8 +376,8 @@ operatorTests = [
 
 otherTests = [
   "y = f 1 'c' 2.3 \"foobar\"",
-  "fact x = if (x == 0) then 1 else (fact x (x - 1))",
-  "fact x = if ((==) 0 x) then 1 else (fact x ((-) x 1))",
+  "fact x = if (x == 0) then 1 else (x * fact (x - 1))",
+  "fact x = if ((==) 0 x) then 1 else (x * fact ((-) x 1))",
   "y x = if x then (if z then q else x) else w",
   "y x1 x2 x3 = if f x1 then g x2 else h x3",
   "y x1 x2 x3 = if x1 then x2 else x3",
