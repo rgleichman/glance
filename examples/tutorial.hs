97cff80df023df0f1cf6e41962811832de110e36
Robbie Gleichman <rgleichman@gmail.com>
2016-12-07 22:09:52 -0800

    Revise tutorial.

--- examples/tutorial.hs
+++ examples/tutorial.hs
@@ -6,7 +6,7 @@ visual programming languages, and that there is a huge universe of  visual
 programming language designs waiting to be discovered.
 
 Let's start with y = f x.
-The function f is applied to the argument x, and the result is  bound to y.
+The function f is applied to the argument x, and the result is bound to y.
 -}
 y = f x
 
@@ -34,7 +34,7 @@ Lines in Glance are largely equivalent to names (variable names, function names,
 etc.) in textual Haskell. Thus if two things are connected with a line in Glance,
 it means they have the same value.
 
-Let's make things a bit more interesing with a nested function application.
+Let's make things a bit more interesting with a nested function application.
 y = (*) ((+) 8 7) 2
 -}
 y = (*) ((+) 8 7) 2
@@ -44,7 +44,7 @@ f x = 3 * x
 
 {- Let's try to figure it out. First look at the f. It's green and in an orange
 box which indicates that it is bound to the result. Now look at the
-function application. It looks like 3 is being multipled by something, but what?
+function application. It looks like 3 is being multiplied by something, but what?
 There is a line connecting the second argument of * to the dot in the green icon,
 so the second line probably originates from the dot in the green icon.
 The result of the function application is also connected to the blue square
@@ -60,13 +60,13 @@ f = 3 * f
 {-Nope, no green icon. The recursion of f is just indicated by a line from f to
 the argument. What construct in Haskell has three ingredients: a name, a variable,
 and a result that derives from the variable?  Here's a hint.
-y has a value of 14.
+y has a value of 21.
 -}
 y = (\x -> 3 * x) 7
 
 {-Now for the answer. As you might have suspected. The green icon defines a
 function. The dot inside the icon is the formal parameter. The blue square
-represents the value returned by function (ie. what's on the right size
+represents the value returned by function (i.e. what's on the right size
 of the -> in a lambda expression). The green circle represents the function
 that has been defined.
 
@@ -81,12 +81,12 @@ f = (\x -> 3 * x)
 {-
 Here, the new function is applied to the argument 7, and the result of the
 function application is bound to y.
-y = (\x -> 3 * x) 7-
+y = (\x -> 3 * x) 7
 -}
 y = (\x -> 3 * x) 7
 
 {-A more complex example:
--f x y =  let q = (f (x + y - 40) x*2 ) in
+-f x y =  let q = ((f (x + y - 40) x) * 2) in
 --  max (2 * y) q
 If you have some drawing implements handy, you might want to try drawing this
 yourself before scrolling down. One place to start is to figure out how functions
@@ -132,17 +132,17 @@ Drawing below:
 -
 -
 -
--f x y =  let q = (f (x + y - 40) x*2 ) in
+-f x y =  let q = ((f (x + y - 40) x) * 2) in
 --  max (2 * y) q
 -}
-f x y =  let q = (f (x + y - 40) x*2 ) in
+f x y =  let q = ((f (x + y - 40) x) * 2) in
   max (2 * y) q
 
-{-Something different about Glance is that Glance does not have any code regions
-where, for example, the icons composing the body of a function would be restricted
-to a rectangle. In Glance, all icons are on the same level. Theoretically,
-Glance's flat layout should allow more compact drawings since space is not wasted
-by extra boxes.
+{-Something different about Glance is that Glance does not have any code regions.
+In other visual programming languages, the icons inside the body of a function
+would be restricted to a rectangle. In Glance, all icons are on the same level.
+Theoretically, Glance's flat layout should allow more compact drawings since space
+is not wasted by extra boxes.
 
 f1 = (\x1 -> (\x2 -> (\x3 -> sum [x1, x2, x3])))
 -}
@@ -152,9 +152,9 @@ f1 = (\x1 -> (\x2 -> (\x3 -> sum [x1, x2, x3])))
 regions.
 
 Regions however can still be useful to tell at what level, or in other words, in
-which function is a parameter used. In the code below for example, it would
+which function a parameter is used. In the code below for example, it would
 probably take some time to figure out that x is only being used in an inner
-function. To address this, I hope to have Glance draw a perimiter around all
+function. To address this, I hope to have Glance draw a perimeter around all
 icons inside a function (including the function's lambda icon). This would occur
 after layout so it would not make the drawing any larger.
 
@@ -169,15 +169,17 @@ and no it does not look graphical. The core idea is that nested function
 application has a tree topology, not a graph topology. The result of each
 sub-expression is only used once. Glance takes advantage of this tree topology
 to make its drawings more compact.
-y = foo (3 + (baz 2)) (8* (baz 9))
+
+y = foo (3 + (baz 2)) (8* (baz 2))
 -}
 y = foo (3 + (baz 2)) (8* (baz 2))
 
 {-As soon as an expression is used more than once, the tree topology is lost,
-and Glance extracts the sub-expression into a seperate (non-nested) icon.
+and Glance extracts the sub-expression into a separate (non-nested) icon.
+
+y = foo (3 + bazOf2) (8* bazOf2) where bazOf2 = baz 2
 -}
-y = foo (3 + bazOf2) (8* bazOf2) where
-  bazOf2 = baz 2
+y = foo (3 + bazOf2) (8* bazOf2) where bazOf2 = baz 2
 
 {-There are many different ways that function application trees can be represented.
 The linear layout Glance currently uses is just the simplest. Large expressions
@@ -186,7 +188,7 @@ Other tree layouts can make these large expressions much more readable.
 
 Glance's drawing of this expression is especially hard to read because the
 apply icon rectangles are all red. Cycling through different colors would probably
-help distingish each icon.
+help distinguish each icon.
 y = (((2 + 4 * 4) - (7+ 2 + baz)*8)/21)
 -}
 y = (((2 + 4 * 4) - (7+ 2 + baz)*8)/21)
@@ -203,7 +205,7 @@ them from the apply icon. In a future iteration of the Glance icons, the match a
 apply icons should be made more dissimilar so that they can not be confused with
 each other, even when displaying Glance drawings in black and white.
 
-Now that you are fammiliar with matches, here's a simple case expression.
+Now that you are familiar with matches, here's a simple case expression.
 --y = case maybeInt of
 --  Just x -> x + 1
 --  Nothing -> 0
@@ -212,7 +214,7 @@ y = case maybeInt of
   Just x -> x + 1
   Nothing -> 0
 
-{-The case icon in the magenta icon with three yellow circles next to it.
+{-The case icon is the magenta icon with three yellow circles next to it.
 The matches (the textual part left of the arrow) connect to the triangles,
 and the result for each match (the textual part to the right of the arrow) is
 indicated with the yellow circle. The result for the second match (Nothing -> 0)
@@ -234,16 +236,16 @@ Guards and if expressions look like this:
 y | x == 0 = 1
   | otherwise = x + 1
 
-{-The boolean expressions (eg. x == 0) connect to the orange Ls, and the
-corresponding result expressions (eg. x + 1) connect to the triangle on the
-other side of the midline. The overall result that the guard is bound to connects
-to either the top or bottom of the midline.
+{-The Boolean expressions (e.g. x == 0) connect to the orange Ls, and the
+corresponding result expressions (e.g. x + 1) connect to the triangle on the
+other side of the mid-line. The overall result that the guard is bound to connects
+to either the top or bottom of the mid-line.
 
 Currently, the guard icon and the case icon look similar since they have similar
 topology (number of connections = 1 + 2 * n), but they should look less similar
 in better icon versions.
 
-If expressions are rendered the same as a guard with only one boolean.
+"If" expressions are rendered the same as a guard with only one Boolean.
 
 --factorial x =
 --  if x == 0
